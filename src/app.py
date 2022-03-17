53d4bf4592ffe3edb8da1a2c2ef482c2b6bc6671
Lucas Pimentel <luk.2001@hotmail.com>
2022-03-17 09:22:26 -0300

    Adds endpoints for the feature
    Co-authored-by: Thiago Lopes <thiago.lopes.santos.tls@gmail.com>
    Co-authored-by: Rhuan Carlos <Rhuancpq@users.noreply.github.com>
    Co-authored-by: joao-henrique10 <joao-henrique10@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_restful import Api
-from src.resources.hello_world import HelloWorld
+from src.resources.hello_world import HelloWorld, HelloWorld2
+from src.resources.available import AvailablePreConfigs
+
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(HelloWorld, "/hello")
+
+api.add_resource(HelloWorld2, "/hello-world")
+
+api.add_resource(AvailablePreConfigs, "/available-pre-configs")
