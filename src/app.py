1f268f067798ce61311840c8006f02104928e00a
igor-paiva <igorbatistapaiva@outlook.com>
2022-03-18 14:06:40 -0300

    Fix MongoDB config import

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from src.resources.hello_world import HelloWorld
 from flask_mongoengine import MongoEngine
-from config import MONGO_SETTINGS
+from .config import MONGO_SETTINGS
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = MONGO_SETTINGS
