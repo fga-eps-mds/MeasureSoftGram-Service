2e1977d6616b69a62bb8c4f53fb0d708df368397
Marcos574 <mvdeus574@gmail.com>
2022-04-04 22:57:48 -0300

    Fix a tests
    Co-authored-by: VitorB2002 <vitorb.santos2002@gmail.com>
    Co-authored-by: LucasPimentel123 <luk.2001@hotmail.com>
    Co-authored-by: Igor Paiva <igorbatistapaiva@outlook.com>
    Co-authored-by: Rhuancpq <rhuancarlos.queiroz@gmail.com>
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>

--- src/app.py
+++ src/app.py
@@ -5,13 +5,31 @@ from src.resources.available import AvailablePreConfigs
 from flask_mongoengine import MongoEngine
 from .config import MONGO_SETTINGS
 
-app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = MONGO_SETTINGS
-api = Api(app)
-db = MongoEngine(app)
 
-api.add_resource(HelloWorld, "/hello")
+def create_app(is_testing=False):
+    """
+    Create the Flask app
+    """
+    app = Flask(__name__)
 
-api.add_resource(HelloWorld2, "/hello-world")
+    if is_testing:
+        app.config["TESTING"] = True
+        app.config["MONGODB_SETTINGS"] = {
+            "host": "mongomock://localhost",
+            "db": "measuresoftgram",
+        }
+    else:
+        app.config["MONGODB_SETTINGS"] = MONGO_SETTINGS
 
-api.add_resource(AvailablePreConfigs, "/available-pre-configs")
+    api = Api(app)
+
+    # FIXME: Create routes file
+    api.add_resource(HelloWorld, "/hello")
+
+    api.add_resource(HelloWorld2, "/hello-world")
+
+    api.add_resource(AvailablePreConfigs, "/available-pre-configs")
+
+    db = MongoEngine(app)
+
+    return app, api, db
--- tests/integration/test_hello_world.py
+++ tests/integration/test_hello_world.py
@@ -1,9 +1,7 @@
-from src.app import app
+from flask import jsonify
 
 
-def test_hello_world():
-    # FIXME - add fixture for app client and other fixtures
-    with app.test_client() as client:
-        response = client.get("/hello")
-        assert response.status_code == 200
-        assert response.data == b'{"Hello":"World!"}\n'
+def test_hello_world(client):
+    response = client.get("/hello")
+    assert response.status_code == 200
+    assert response.data == b'{"Hello":"World!"}\n'
