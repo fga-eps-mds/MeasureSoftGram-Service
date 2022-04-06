cacf37fcd9f9114dc5f4c6565f4a6103dd71b65a
VitorB2002 <vitorb.santos2002@gmail.com>
2022-04-06 17:29:05 -0300

    Add integration test and fix indentations
    Signed-off-by: VitorB2002 <vitorb.santos2002@gmail.com>
    Co-authored-by: Marcos574 <mvdeus574@gmail.com>

--- tests/integration/test_metrics.py
+++ tests/integration/test_metrics.py
@@ -21,8 +21,11 @@ def test_pre_config_metric_post(client):
     }
 
     response = client.post("/pre-config-metrics", json=dict)
-    message = '{"comment_lines_density": 0.2, "complexity": 0.5, "coverage": 0.2, "duplicated_lines_density": 0.3, "files": 0.8, "functions": 0.5, "ncloc": 0.9, "security_rating": 0.1, "test_errors": 0.2, "test_execution_time": 0.1, "test_failures": 0.3, "tests": 0.2}' + '\n'
+    message = '{"metrics_list": {"comment_lines_density": 0.2, "complexity": 0.5, "coverage": 0.2, "duplicated_lines_density": 0.3, "files": 0.8, "functions": 0.5, "ncloc": 0.9, "security_rating": 0.1, "test_errors": 0.2, "test_execution_time": 0.1, "test_failures": 0.3, "tests": 0.2}' + '}\n'
     byte_message = bytes(message, 'utf-8')
+    print(response.data)
+    print("\n\n")
+    print(byte_message)
     assert response.status_code == 201
     assert response.data == byte_message
 
@@ -51,6 +54,48 @@ def test_wrong_file_post(client):
 
     dict = {
         "id_wanted": "fi2ng9248",
+        "comment_lines_density": 0.2,
+        "complexity": 0.5,
+        "coverage": 0.2,
+        "duplicated_lines_density": 0.3,
+        "files": 0.8,
+        "functions": 0.5,
+        "ncloc": 0.9,
+        "security_rating": 0.1,
+        "test_errors": 0.2,
+        "test_execution_time": 0.1,
+        "test_failures": 0.3,
+        "tests": 0.2
+    }
+
+    response = client.post("/pre-config-metrics", data=dict)
+    assert response.status_code == 400
+
+def test_invalid_id(client):
+
+    dict = {
+        "id_wanted": None,
+        "comment_lines_density": 0.2,
+        "complexity": 0.5,
+        "coverage": 0.2,
+        "duplicated_lines_density": 0.3,
+        "files": 0.8,
+        "functions": 0.5,
+        "ncloc": 0.9,
+        "security_rating": 0.1,
+        "test_errors": 0.2,
+        "test_execution_time": 0.1,
+        "test_failures": 0.3,
+        "tests": 0.2
+    }
+
+    response = client.post("/pre-config-metrics", data=dict)
+    assert response.status_code == 400
+
+def test_unsaved_id(client):
+
+    dict = {
+        "id_wanted": "id_não_existente",
         "comment_lines_density": 0.2,
         "complexity": 0.5,
         "coverage": 0.2,
