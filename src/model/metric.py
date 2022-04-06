f7b45ecb142b90967ca5a36b679028eb5ddbab8e
VitorB2002 <vitorb.santos2002@gmail.com>
2022-04-06 17:27:54 -0300

    Upadte models to save the whole dict in database
    Signed-off-by: VitorB2002 <vitorb.santos2002@gmail.com>
    Co-authored-by: Marcos574 <mvdeus574@gmail.com>

--- src/model/metric.py
+++ src/model/metric.py
@@ -1,34 +1,12 @@
 from flask import Flask, request, jsonify
-import json
 import mongoengine as me
 
 
 class Metrics(me.Document):
 
-    comment_lines_density = me.FloatField()
-    complexity = me.FloatField()
-    coverage = me.FloatField()
-    duplicated_lines_density = me.FloatField()
-    files = me.FloatField()
-    functions = me.FloatField()
-    ncloc = me.FloatField()
-    security_rating = me.FloatField()
-    test_errors = me.FloatField()
-    test_execution_time = me.FloatField()
-    test_failures = me.FloatField()
-    tests = me.FloatField()
+    metrics_list = me.DictField()
 
     def to_json(self):
-        return {"comment_lines_density": self.comment_lines_density,
-                "complexity": self.complexity,
-                "coverage": self.coverage,
-                "duplicated_lines_density": self.duplicated_lines_density,
-                "files": self.files,
-                "functions": self.functions,
-                "ncloc": self.ncloc,
-                "security_rating": self.security_rating,
-                "test_errors": self.test_errors,
-                "test_execution_time": self.test_execution_time,
-                "test_failures": self.test_failures,
-                "tests": self.tests
-                }
+        return {
+            "metrics_list": self.metrics_list
+        }
