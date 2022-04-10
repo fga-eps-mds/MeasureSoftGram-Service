76f2d4da4055874eef7a7b867bdb0a109996402b
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-04-10 17:00:18 -0300

    Add others fields to PreConfig
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>
    Co-authored-by: igor-paiva <igorbatistapaiva@outlook.com>
    Co-authored-by: RCristyan <rcristyan9@gmail.com>

--- src/model/pre_config.py
+++ src/model/pre_config.py
@@ -4,12 +4,26 @@ import mongoengine as me
 
 
 class PreConfig(me.Document):
-    name = me.StringField()
+    name = me.StringField(unique=True)
     characteristics = me.ListField()
+    subcharacteristics = me.ListField()
+    measures = me.ListField()
+    characteristics_weights = me.ListField()
+    subcharacteristics_weights = me.ListField()
+    measures_weights = me.ListField()
+
+    def created_at(self):
+        return self.id.generation_time
 
     def to_json(self):
         return {
+            "_id": str(self.pk),
             "name": self.name,
             "characteristics": self.characteristics,
-            "_id": str(self.pk),
+            "subcharacteristics": self.subcharacteristics,
+            "measures": self.measures,
+            "characteristics_weights": self.characteristics_weights,
+            "subcharacteristics_weights": self.subcharacteristics_weights,
+            "measures_weights": self.measures_weights,
+            "created_at": self.created_at(),
         }
--- src/resources/pre_config.py
+++ src/resources/pre_config.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource
 from src.model.pre_config import PreConfig
+import requests
+import mongoengine as me
 
 
 class PreConfigs(Resource):
     def post(self):
         data = request.get_json(force=True)
 
-        pre_config = PreConfig(
-            name=data["name"], characteristics=data["characteristics"]
-        ).save()
+        try:
+            pre_config = PreConfig(data).save()
+        except me.errors.ValidationError:
+            return {
+                "error": "The pre config name is already in use"
+            }, requests.codes.unprocessable_entity
 
-        return pre_config.to_json(), 201
+        return pre_config.to_json(), requests.codes.created
