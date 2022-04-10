90148281a65db73509419f942c03fd8a8d6a1987
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-10 17:48:52 -0300

    Add tests to pre config creation
    Co-authored-by: Rhuancpq <rhuancarlos.queiroz@gmail.com>
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>
    Co-authored-by: VitorB2002 <vitorb.santos2002@gmail.com>
    Co-authored-by: Marcos574 <mvdeus574@gmail.com>

--- tests/integration/test_preconfig.py
+++ tests/integration/test_preconfig.py
@@ -1,23 +1,44 @@
-import json
-from urllib import response
-from flask import jsonify
+from src.model.pre_config import PreConfig
 
-user_characteristics = ["maintanability"]
+EMPTY_LEVELS_CREATE_PRE_CONFIG_PARAMS = {
+    "characteristics": [""],
+    "subcharacteristics": [""],
+    "measures": [""],
+    "characteristics_weights": [""],
+    "subcharacteristics_weights": [""],
+    "measures_weights": [""],
+}
 
-data = {"characteristics": user_characteristics, "name": "pre_config_name"}
 
+def test_create_pre_config_success(client):
+    params = {"name": "Pre config 1", **EMPTY_LEVELS_CREATE_PRE_CONFIG_PARAMS}
 
-def test_preconfig(client):
-    response = client.post(
-        "/pre-configs",
-        json=data,
-    )
+    response = client.post("/pre-configs", json=params)
+
+    response_json = dict(response.json)
+    response_json.pop("created_at", None)
+    pre_config_id = response_json.pop("_id", None)
+
     assert response.status_code == 201
+    assert response_json == params
+    assert PreConfig.objects.with_id(pre_config_id) is not None
+
+
+def test_create_pre_config_not_unique_name(client):
+    pre_config_one = PreConfig(name="Name one")
+    pre_config_one.save()
+
+    params = {"name": "Name one", **EMPTY_LEVELS_CREATE_PRE_CONFIG_PARAMS}
+
+    response = client.post("/pre-configs", json=params)
+
+    assert response.status_code == 422
+    assert response.json == {"error": "The pre config name is already in use"}
 
 
 def test_preconfig_wrong_path(client):
     response = client.post(
         "/selecte-pre-config",
-        json=data,
+        json={},
     )
     assert response.status_code == 404
