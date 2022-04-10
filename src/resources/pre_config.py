789132297487c59e84ec0189da0c40c1dc1702c5
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-10 17:43:17 -0300

    Fix model params and not unique error in pre configs resource
    Co-authored-by: Rhuancpq <rhuancarlos.queiroz@gmail.com>
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>
    Co-authored-by: VitorB2002 <vitorb.santos2002@gmail.com>
    Co-authored-by: Marcos574 <mvdeus574@gmail.com>

--- src/resources/pre_config.py
+++ src/resources/pre_config.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import request
 from flask_restful import Resource
 from src.model.pre_config import PreConfig
 import requests
@@ -6,12 +6,23 @@ import mongoengine as me
 
 
 class PreConfigs(Resource):
+    def get(self):
+        return PreConfig.objects().to_json(), 200
+
     def post(self):
         data = request.get_json(force=True)
 
         try:
-            pre_config = PreConfig(data).save()
-        except me.errors.ValidationError:
+            pre_config = PreConfig(
+                name=data["name"],
+                characteristics=data["characteristics"],
+                subcharacteristics=data["subcharacteristics"],
+                measures=data["measures"],
+                characteristics_weights=data["characteristics_weights"],
+                subcharacteristics_weights=data["subcharacteristics_weights"],
+                measures_weights=data["measures_weights"],
+            ).save()
+        except me.errors.NotUniqueError:
             return {
                 "error": "The pre config name is already in use"
             }, requests.codes.unprocessable_entity
