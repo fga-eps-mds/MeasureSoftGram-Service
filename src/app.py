4106c60b4e6e8341e78801bc6722a3e189b6a901
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-11 13:07:04 -0300

    Change metrics resource file name

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from src.resources.available import AvailablePreConfigs
 from src.resources.pre_config import PreConfigs
+from src.resources.import_metrics import ImportMetrics
 from flask_mongoengine import MongoEngine
 from .config import MONGO_SETTINGS
 
--- src/resources/import_metrics.py
+++ src/resources/import_metrics.py
@@ -1,12 +1,15 @@
 from flask_restful import Resource
 from src.model.metrics_component_tree import MetricsComponentTree
 from src.model.pre_config import PreConfig
-from flask import request
+from flask import request, jsonify
 import mongoengine as me
 import requests
 
 
 class ImportMetrics(Resource):
+    def get(self):
+        return PreConfig.objects.with_id("625439e55742575d765b3c22").to_json(), 200
+
     def post(self):
         data = request.get_json(force=True)
 
@@ -18,9 +21,11 @@ class ImportMetrics(Resource):
         except me.errors.ValidationError:
             return requests.codes.not_found
 
-        MetricsComponentTree(
-            pre_config_id=data.pop("pre_config_id"),
-            components=data["components"]
-        ).save()
+        try:
+            MetricsComponentTree(
+                pre_config_id=data.pop("pre_config_id"), components=data["components"]
+            ).save()
+        except me.errors.ValidationError as error:
+            return error.to_dict(), requests.codes.unprocessable_entity
 
         return requests.codes.created
