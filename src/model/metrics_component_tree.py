f35f44ce9711d78d1701f55090e20c8b01ccd019
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-11 20:08:26 -0300

    Refactor metrics validation to be less complex
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>
    Co-authored-by: VitorB2002 <vitorb.santos2002@gmail.com>
    Co-authored-by: Marcos574 <mvdeus574@gmail.com>
    Co-authored-by: Slimow <170114139@aluno.unb.br>
    Co-authored-by: LucasPimentel123 <luk.2001@hotmail.com>
    Co-authored-by: joao-henrique10 <jhcalzavara@hotmail.com>

--- src/model/metrics_component_tree.py
+++ src/model/metrics_component_tree.py
@@ -4,37 +4,45 @@ from src.util.constants import CORE_URL
 from src.model.pre_config import PreConfig
 
 
+def flatten_list(array):
+    return [item for sublist in array for item in sublist]
+
+
+def get_required_metrics(pre_config_id):
+    available_entries = requests.get(
+        CORE_URL + "/available-pre-configs",
+        headers={"Accept": "application/json"},
+    ).json()
+
+    pre_config = PreConfig.objects.with_id(pre_config_id)
+
+    required_metrics = []
+
+    for measure in pre_config.measures:
+        for metric in available_entries["measures"][measure]["metrics"]:
+            required_metrics.append(metric)
+
+    return list(dict.fromkeys(required_metrics))
+
+
 class MetricsComponentTree(me.Document):
 
     pre_config_id = me.StringField()
     components = me.ListField()
 
     def clean(self):
-        available_entries = requests.get(
-            CORE_URL + "/available-pre-configs",
-            headers={"Accept": "application/json"},
-        ).json()
-
-        pre_config = PreConfig.objects.with_id(self.pre_config_id)
-
         found_metrics = []
         missing_metrics = []
-        required_metrics = []
+        required_metrics = get_required_metrics(self.pre_config_id)
 
-        for measure in pre_config.measures:
-            for metric in available_entries["measures"][measure]["metrics"]:
-                required_metrics.append(metric)
-
-        all_metrics = list(
+        all_file_metrics = list(
             map(
                 lambda item: list(map(lambda itm: itm["metric"], item["measures"])),
                 self.components,
             )
         )
 
-        found_metrics = dict.fromkeys(
-            [item for sublist in all_metrics for item in sublist], True
-        )
+        found_metrics = dict.fromkeys(flatten_list(all_file_metrics), True)
 
         for required_metric in required_metrics:
             if not found_metrics.get(required_metric, False):
@@ -45,7 +53,7 @@ class MetricsComponentTree(me.Document):
 
             raise me.errors.ValidationError(
                 "The metrics in this file are not the expected in the pre config."
-                + f"Missing metrics: {missing_text}."
+                + f" Missing metrics: {missing_text}."
             )
 
     def to_json(self):
