200b63ddba15c7d3fba460388ae340a144f87d11
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-11 13:07:28 -0300

    Add metrics validation to MetricsComponentTree model
    Co-authored-by: Rhuancpq <rhuancarlos.queiroz@gmail.com>
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>
    Co-authored-by: Marcos574 <mvdeus574@gmail.com>
    Co-authored-by: Frans6 <jefferson.frds@gmail.com>
    Co-authored-by: VitorB2002 <vitorb.santos2002@gmail.com>

--- src/model/metrics_component_tree.py
+++ src/model/metrics_component_tree.py
@@ -1,6 +1,7 @@
 import mongoengine as me
 import requests
 from src.util.constants import CORE_URL
+from src.model.pre_config import PreConfig
 
 
 class MetricsComponentTree(me.Document):
@@ -15,21 +16,40 @@ class MetricsComponentTree(me.Document):
         ).json()
 
         # Reduzir todas as metricas necessárias para a pré config relacionada em um vetor
-        # PreConfig.with_id(self.pre_config_id)
-        all_metrics = [
-            key for key in available_entries["measures"].keys()
-        ]
+        pre_config = PreConfig.objects.with_id(self.pre_config_id)
 
-        [key for key in {"name": "asd", "age": 12}.keys()]
+        found_metrics = []
+        required_metrics = []
 
-        all_metrics
+        for measure in pre_config.measures:
+            for metric in available_entries["measures"][measure]["metrics"]:
+                required_metrics.append(metric)
 
         # A partir do vetor verificar se existe ao menos um component que possua
         # a métrica em questão
+        for required_metric in required_metrics:
+            for component in self.components:
+                for measure in component["measures"]:
+                    found = measure["metric"] == required_metric
 
-        raise me.errors.ValidationError(
-            "The metrics in this file are not the expected in the pre config"
-        )
+                    if found:
+                        break
+
+                if found:
+                    break
+
+            found_metrics.append(found)
+
+        if not all(found_metrics):
+            msg = "Missing metrics: "
+
+            for idx in range(len(found_metrics)):
+                if not found_metrics[idx]:
+                    msg = f"{msg} {required_metrics[idx]},"
+
+            raise me.errors.ValidationError(
+                f"The metrics in this file are not the expected in the pre config. {msg[:-1]}"
+            )
 
     def to_json(self):
         return {"pre_config_id": self.pre_config_id, "components": self.components}
