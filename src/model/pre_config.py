71bc3bef9e6a981cc6bc8b8ae7763396c0d13906
Samuel Macedo <81116971+Slimow@users.noreply.github.com>
2022-04-16 18:51:05 -0300

    Add to lean json format and to goal json format
    Co-authored-by: joao-henrique10 <jhcalzavara@hotmail.com>
    Co-authored-by: Frans6 <jefferson.frds@gmail.com>

--- src/model/pre_config.py
+++ src/model/pre_config.py
@@ -19,3 +19,20 @@ class PreConfig(me.Document):
             "measures": self.measures,
             "created_at": str(self.created_at()),
         }
+
+    def to_lean_json(self):
+        return{
+            "_id": str(self.pk),
+            "name": self.name,
+            "created_at": str(self.created_at()),
+        }
+
+    def to_goal_json(self):
+        return{
+            "characteristics": self.characteristics,
+            "subcharacteristics": self.subcharacteristics,
+            "measures": self.measures,
+            "characteristics_weights": self.characteristics_weights,
+            "subcharacteristics_weights": self.subcharacteristics_weights,
+            "measures_weights": self.measures_weights,
+        }
