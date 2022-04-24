035f64cfc88238d3c7fdb79c8c2f5dd36846bda7
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-24 12:12:20 -0300

    Refactor show and list pre configs test
    Co-authored-by: joao-henrique10 <jhcalzavara@hotmail.com>

--- tests/integration/test_preconfig.py
+++ tests/integration/test_preconfig.py
@@ -16,6 +16,32 @@ EMPTY_LEVELS_PRE_CONFIG_LEAN_PARAMS = {
     "created_at": [""],
 }
 
+PRE_CONFIG_FIELDS_EXAMPLE = {
+    "characteristics": {
+        "reliability": {
+            "weight": 50,
+            "subcharacteristics": ["testing_status"],
+            "weights": {"testing_status": 100.0},
+        },
+        "maintainability": {
+            "weight": 50,
+            "subcharacteristics": ["modifiability"],
+            "weights": {"modifiability": 100.0},
+        },
+    },
+    "subcharacteristics": {
+        "testing_status": {
+            "weights": {"passed_tests": 100.0},
+            "measures": ["passed_tests"],
+        },
+        "modifiability": {
+            "weights": {"non_complex_file_density": 100.0},
+            "measures": ["non_complex_file_density"],
+        },
+    },
+    "measures": ["passed_tests", "non_complex_file_density"],
+}
+
 
 def test_create_pre_config_success(client):
     params = {"name": "Pre config 1", **EMPTY_LEVELS_CREATE_PRE_CONFIG_PARAMS}
@@ -43,63 +69,8 @@ def test_create_pre_config_not_unique_name(client):
     assert response.json == {"error": "The pre config name is already in use"}
 
 
-""""{  
-        "characteristics": {
-        "reliability": {
-            "expected_value": 70,
-            "weight": 50,
-            "subcharacteristics": ["testing_status"],
-            "weights": {"testing_status": 100.0}
-        },
-        "maintainability": {
-            "expected_value": 30,
-            "weight": 50,
-            "subcharacteristics": ["modifiability"],
-            "weights": {"modifiability": 100.0}
-        }
-    },
-    "subcharacteristics": {
-        "testing_status": {
-            "weights": {"passed_tests": 100.0},
-            "measures": ["passed_tests"]
-        },
-        "modifiability": {
-            "weights": {"non_complex_file_density": 100.0},
-            "measures": ["non_complex_file_density"]
-        }
-    },
-    "measures": ["passed_tests", "non_complex_file_density"]
-}
-""" ""
-
-
 def test_pre_config(client):
-    pre_config_one = PreConfig(
-        name="abc",
-        characteristics={
-            "reliability": {
-                "weight": 50,
-                "subcharacteristics": ["testing_status"],
-                "weights": {"testing_status": 100.0},
-            },
-            "maintainability": {
-                "weight": 50,
-                "subcharacteristics": ["modifiability"],
-                "weights": {"modifiability": 100.0},
-            },
-        },
-        subcharacteristics={
-            "testing_status": {
-                "weights": {"passed_tests": 100.0},
-                "measures": ["passed_tests"],
-            },
-            "modifiability": {
-                "weights": {"non_complex_file_density": 100.0},
-                "measures": ["non_complex_file_density"],
-            },
-        },
-        measures=["passed_tests", "non_complex_file_density"],
-    )
+    pre_config_one = PreConfig(name="abc", **PRE_CONFIG_FIELDS_EXAMPLE)
     pre_config_one.save()
 
     response = client.get("/pre-configs")
@@ -107,65 +78,18 @@ def test_pre_config(client):
     assert response.status_code == 200
 
     all_pre_configs = [x.to_lean_json() for x in PreConfig.objects.all()]
+
     assert response.json == all_pre_configs
 
 
-""" 
-
-Testar o endpoint /pre-configs
-
-- Mockar pre configs no banco de dados, ou seja criar pre configs "falsas" no BD
-
-- Realizar a chamada do endpoint client.get("/pre-configs")
-
-- Dar os asserts necessários
-
-
-Testar o endpoint /pre-configs/<pre_config_id>
-
-- Mockar uma pre config no BD com o mesmo id que vai ser chamado
-
-- Realizar a chamada no endpoint client.get("/pre-configs/id_fake_escolhido")
-
-- Dar os asserts
-
-"""
-
-
 def test_unique_pre_config(client):
-    pre_config_two = PreConfig(
-        name="def",
-        characteristics={
-            "reliability": {
-                "weight": 50,
-                "subcharacteristics": ["testing_status"],
-                "weights": {"testing_status": 100.0},
-            },
-            "maintainability": {
-                "weight": 50,
-                "subcharacteristics": ["modifiability"],
-                "weights": {"modifiability": 100.0},
-            },
-        },
-        subcharacteristics={
-            "testing_status": {
-                "weights": {"passed_tests": 100.0},
-                "measures": ["passed_tests"],
-            },
-            "modifiability": {
-                "weights": {"non_complex_file_density": 100.0},
-                "measures": ["non_complex_file_density"],
-            },
-        },
-        measures=["passed_tests", "non_complex_file_density"],
-    )
-    pre_config_two.save()
+    pre_config = PreConfig(name="def", **PRE_CONFIG_FIELDS_EXAMPLE)
+    pre_config.save()
 
-    response = client.get("/pre-configs/" + str(pre_config_two.pk))
+    response = client.get(f"/pre-configs/{pre_config.pk}")
 
     assert response.status_code == 200
-
-    assert response.json == pre_config_two.to_json()
+    assert response.json == pre_config.to_json()
 
 
 def test_preconfig_lean_wrong_path(client):
@@ -173,4 +97,5 @@ def test_preconfig_lean_wrong_path(client):
         "/pre-config",
         json={},
     )
+
     assert response.status_code == 404
