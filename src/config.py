02cf3c0c3d6ebc1383d02c9f392a3befddf8f011
Durval Carvalho <durval.carvalho@brelo.com.br>
2022-06-23 21:48:11 -0300

    Set DB_HOST and DB_PORT enviroment vars

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 import os
 
 MONGO_SETTINGS = {
-    "host": "measuresoftgram-db",
-    "port": 27017,
+    "host": os.getenv("DB_HOST", "measuresoftgram-db"),
+    "port": int(os.getenv("DB_PORT", "27017")),
     "db": os.getenv("DB_NAME", "measuresoftgram"),
     "username": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", "example"),
