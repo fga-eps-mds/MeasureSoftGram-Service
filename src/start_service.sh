5ea2c6338f1f474854fdaea650d4ac1a7b68e60e
Durval Carvalho <durval.carvalho@brelo.com.br>
2022-07-12 09:03:47 -0300

    Setup Django project

---
+++ src/start_service.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Exporting all environment variables to use in crontab
+env | sed 's/^\(.*\)$/ \1/g' > /root/env
+
+# Função que espera o postgres ficar pronto antes de subir o server
+function_postgres_ready() {
+python << END
+import socket
+import time
+import os
+
+port = int(os.environ["POSTGRES_PORT"])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(('db', port))
+s.close()
+END
+}
+
+# Dev only. Quando o desenvolvimento estiver pronto não é preciso instalar as dependências toda vez que o container subir
+echo '======= CHECKING FOR UNINSTALLED PKGs AND INSTALLING'
+pip install -r requirements.txt
+
+until function_postgres_ready; do
+  >&2 echo "======= POSTGRES IS UNAVAILABLE, WAITING"
+  sleep 1
+done
+echo "======= POSTGRES IS UP, CONNECTING"
+
+echo '======= RUNNING MIGRATIONS'
+python3 manage.py migrate
+
+# echo '======= PREPOPULATING THE DATABASE'
+# python3 manage.py load_initial_data
+
+echo '======= RUNNING SERVER'
+python3 manage.py runserver 0.0.0.0:80
