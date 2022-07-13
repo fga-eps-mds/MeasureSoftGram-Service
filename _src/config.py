6322bd0724b08c9d1ac7043f3edd64a9b491afbc
Durval Carvalho <durval.carvalho@brelo.com.br>
2022-07-13 12:03:21 -0300

    Remove flask files

--- _src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-MONGO_SETTINGS = {
-    "host": "measuresoftgram-db",
-    "port": 27017,
-    "db": os.getenv("DB_NAME", "measuresoftgram"),
-    "username": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "example"),
-}
