6f543b522b063143448b650613117970978e8374
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-07-15 09:50:25 -0300

    #108 Visualizar medidas de um repositório mockado

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from core.views import get_mocked_repository
+from core import views
 
 urlpatterns = [
-    path('organizations/1/projects/1/', get_mocked_repository)
+    path('organizations/1/projects/1/', views.get_mocked_repository),
+    path('organizations/1/projects/1/measures/', views.get_mocked_measures)
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -12,3 +12,50 @@ def get_mocked_repository(request):
         'created_at': '2022-07-14T020:00:55.603466',
         'updated_at': '2022-07-15T08:58:55.603466'
     })
+
+
+@api_view(['GET'])
+def get_mocked_measures(request):
+    return Response({
+        'count': 5,
+        'next': None,
+        'previous': None,
+        'results': [
+            {
+                'id': 1,
+                'name': 'non_complex_file_density',
+                'value': 0.45,
+                'created_at': '2022-07-12T14:50:50.888777'
+            },
+            {
+                'id': 2,
+                'name': 'commented_file_density',
+                'value': 0.69,
+                'created_at': '2022-07-12T14:50:50.888777'
+            },
+            {
+                'id': 3,
+                'name': 'duplication_absense',
+                'value': 0.8,
+                'created_at': '2022-07-12T14:50:50.888777'
+            },
+            {
+                'id': 4,
+                'name': 'passed_tests',
+                'value': 0.3,
+                'created_at': '2022-07-12T14:50:50.888777'
+            },
+            {
+                'id': 5,
+                'name': 'test_builds',
+                'value': 0.58,
+                'created_at': '2022-07-12T14:50:50.888777'
+            },
+            {
+                'id': 6,
+                'name': 'test_coverage',
+                'value': 0.92,
+                'created_at': '2022-07-12T14:50:50.888777'
+            },
+        ]
+    })
