344fcf690206e35c6f969e036f20137b7a2e8219
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-07-15 09:51:07 -0300

    #108 Troca project por repository nas urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('organizations/1/projects/1/', views.get_mocked_repository),
-    path('organizations/1/projects/1/measures/', views.get_mocked_measures)
+    path('organizations/1/repository/1/', views.get_mocked_repository),
+    path('organizations/1/repository/1/measures/', views.get_mocked_measures)
 ]
