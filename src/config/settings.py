aba514b602748731b624eb15239b628a9843ea75
Durval Carvalho <durval.carvalho@brelo.com.br>
2022-07-15 21:04:30 -0300

    Associar o modo Debug com uma variável de ambiente

--- src/config/settings.py
+++ src/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "t", "1")
 
 ALLOWED_HOSTS = ['*']
 
