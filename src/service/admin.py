d07353b21a6f5248b46b28b9e409abb8ad36aa35
Herick Portugues <herick.portugues@gmail.com>
2022-07-31 17:18:14 -0300

    #144 Create model CalculatedSubCharacteristic

--- src/service/admin.py
+++ src/service/admin.py
@@ -2,12 +2,9 @@ import datetime as dt
 
 from django.contrib import admin
 
-from service.sub_admin.characteristics import SupportedCharacteristicAdmin
-
 # Do not delete this imports
 from service.sub_admin.measures import CalculatedMeasureAdmin, SupportedMeasureAdmin
-
-# Do not delete this imports
 from service.sub_admin.metrics import CollectedMetricAdmin, SupportedMetricAdmin
 from service.sub_admin.pre_config import PreConfigAdmin
-from service.sub_admin.subcharacteristics import SupportedSubCharacteristicAdmin
+from service.sub_admin.subcharacteristics import SupportedSubCharacteristicAdmin, CalculatedSubCharacteristicAdmin
+from service.sub_admin.characteristics import SupportedCharacteristicAdmin
--- src/service/models.py
+++ src/service/models.py
@@ -1,13 +1,10 @@
 from django.contrib.auth import get_user_model
 
-from service.sub_models.characteristics import SupportedCharacteristic
-
 # Do not delete this imports
 from service.sub_models.measures import CalculatedMeasure, SupportedMeasure
-
-# Do not delete this imports
 from service.sub_models.metrics import CollectedMetric, SupportedMetric
 from service.sub_models.pre_config import PreConfig
-from service.sub_models.subcharacteristics import SupportedSubCharacteristic
+from service.sub_models.subcharacteristics import SupportedSubCharacteristic, CalculatedSubCharacteristic
+from service.sub_models.characteristics import SupportedCharacteristic
 
 User = get_user_model()
