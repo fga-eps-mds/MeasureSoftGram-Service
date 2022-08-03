b6595ea26297ab17dbe051a1b27faf6f16078840
Durval Carvalho <durval.carvalho@brelo.com.br>
2022-08-03 11:47:23 -0300

    Modify test lib to pytest

--- src/utils/tests.py
+++ src/utils/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from utils import chunkify
+
+
+class UtilsModuleTestCase(TestCase):
+    def test_chunkify(self):
+        l_100 = list(range(100))
+        self.assertEqual(
+            chunkify(l_100, 10),
+            [
+                tuple(range(10)), tuple(range(10, 20)), tuple(range(20, 30)),
+                tuple(range(30, 40)), tuple(range(40, 50)), tuple(range(50, 60)),
+                tuple(range(60, 70)), tuple(range(70, 80)), tuple(range(80, 90)),
+                tuple(range(90, 100)),
+            ],
+        )
