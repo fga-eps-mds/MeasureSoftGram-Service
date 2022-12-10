085d49ef114e08aebde664a1b2a3c6df48ceb832
Joao-Moura <jaummoura15@gmail.com>
2022-12-10 15:58:12 -0300

    Adding new views to login and create account

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, re_path
 
-from accounts.views import GithubLoginViewSet, LogoutViewSet
+from accounts.views import (
+    GithubLoginViewSet, LogoutViewSet,
+    CreateAccountViewSet, LoginViewSet
+)
 
 
 urlpatterns = [
     re_path(r'^accounts/', include([
+        re_path(r'^signin/$', CreateAccountViewSet.as_view({'post': 'create'}), name='accounts-signin'),
+        re_path(r'^login/$', LoginViewSet.as_view({'post': 'create'}), name='accounts-login'),
         re_path(r'^logout/$', LogoutViewSet.as_view({'delete': 'destroy'}), name='accounts-logout'),
         re_path(r'^github/login/$', GithubLoginViewSet.as_view(), name='github-login')
     ]))
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -9,17 +9,48 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 from dj_rest_auth.registration.views import SocialLoginView
 
+from accounts.models import CustomUser
+from accounts.serializers import AccountsCreateSerializer, AccountsLoginSerializer
+
 
 class GithubLoginViewSet(SocialLoginView):
+    """
+    ViewSet para login via OAuth2 do Github
+    """
     adapter_class = GitHubOAuth2Adapter
     callback_url = settings.LOGIN_REDIRECT_URL
     client_class = OAuth2Client
+
+
+class CreateAccountViewSet(
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    """
+    ViewSet para criação de conta
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = AccountsCreateSerializer
+
+
+class LoginViewSet(
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    """
+    ViewSet para login de conta
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = AccountsLoginSerializer
 
 
 class LogoutViewSet(
     mixins.DestroyModelMixin,
     viewsets.GenericViewSet
 ):
+    """
+    ViewSet para logout de conta
+    """
     permission_classes = (IsAuthenticated,)
 
     def destroy(self, request, *args, **kwargs):
