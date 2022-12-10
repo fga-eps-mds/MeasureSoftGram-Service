a99b7a8df64d52dfde0f5f62cbfd666def45bc6a
Joao-Moura <jaummoura15@gmail.com>
2022-12-10 14:41:27 -0300

    Adding new tests to logout

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from rest_framework.reverse import reverse
+from rest_framework.authtoken.models import Token
+
+from utils.tests import APITestCaseExpanded
+
+User = get_user_model()
+
+
+class AccountsViews(APITestCaseExpanded):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username='Test-User', first_name='Test',
+            last_name='User', email='test_user@email.com'
+        )
+
+    def test_logout_acccount(self):
+        token = Token.objects.create(user=self.user)
+        self.client.force_authenticate(user=self.user, token=token)
+
+        url = reverse('accounts-logout')
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Token.objects.count(), 0)
+
+    def test_fail_logout_without_authentication(self):
+        url = reverse('accounts-logout')
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 403)
