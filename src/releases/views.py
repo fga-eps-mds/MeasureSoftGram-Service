ac1573e3a5d04d705a231862457523c3ee8087a6
LamequeFernandes <lamequesao@gmail.com>
2023-11-21 19:43:15 -0300

    feat: creating endpoint to check release parameters

--- src/releases/views.py
+++ src/releases/views.py
@@ -3,6 +3,8 @@ from releases.models import Release
 
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -18,3 +20,40 @@ class CreateReleaseModelViewSet(viewsets.ModelViewSet):
         product_key = self.kwargs['product_pk']
 
         return Release.objects.filter(product=product_key)
+
+
+    @action(detail=False, methods=['get'], url_path='is-valid')
+    def check_release(self, request, *args, **kwargs):
+        product_key = self.kwargs['product_pk']
+
+        name_release = request.query_params.get('nome')
+        init_date = request.query_params.get('dt-inicial')
+        final_date = request.query_params.get('dt-final')
+
+        release = Release.objects.filter(
+            product=product_key, release_name=name_release,
+        ).first()
+
+        if release:
+            return Response(
+                data={'detail': 'Já existe uma release com este nome'}, 
+                status=400
+            )
+
+        release = Release.objects.filter(
+            product=product_key, 
+            start_at__gte=init_date,
+            start_at__lte=final_date,
+            end_at__gte=init_date,
+            end_at__lte=final_date
+        ).first()
+
+        if release:
+            return Response(
+                data={'detail': 'Já existe uma release neste período'}, 
+                status=400
+            )
+
+        return Response(
+            {'message': 'Parametros válidos para criação de Release'}
+        )
