05985cbb5ecfc1447340b46bbee3caebac1f3153
LamequeFernandes <lamequesao@gmail.com>
2023-11-22 17:39:32 -0300

    test: tests for different types of release creation

--- src/releases/tests/test_releases_endpoints.py
+++ src/releases/tests/test_releases_endpoints.py
@@ -42,3 +42,167 @@ class ReleaseEndpointsTestCase(APITestCaseExpanded):
             },
         )
     
+    def test_create_new_release_without_description(self):
+        data = {
+            "release_name": "testezada",
+            "start_at": "2023-11-24",
+            "end_at": "2023-11-25",
+            "goal": self.goal.id
+        }
+
+        response = self.client.post(
+            path=f'/api/v1/organizations/{self.org.id}/products/{self.product.id}/create/release/',
+            data=data,
+            format="json"
+        )
+        self.assertEqual(response.status_code, 201)
+        
+        response_json = response.json()
+        self.assertEqual(response_json["release_name"], "testezada")
+        self.assertEqual(response_json["description"], None)
+        self.assertEqual(response_json["start_at"], f"{data['start_at']}T00:00:00-03:00")
+        self.assertEqual(response_json["end_at"], f"{data['end_at']}T00:00:00-03:00")
+        self.assertEqual(response_json["created_by"], self.user.id)
+        self.assertEqual(response_json["product"], self.product.id)
+        self.assertEqual(response_json["goal"], data['goal'])
+        self.assertEqual(response_json["description"], None)
+
+    def test_create_new_release_full(self):
+        data = {
+            "release_name": "testezada 2",
+            "start_at": "2023-11-24",
+            "end_at": "2023-11-25",
+            "goal": self.goal.id,
+            "description": "Apenas um testezinho"
+        }
+
+        response = self.client.post(
+            path=f'/api/v1/organizations/{self.org.id}/products/{self.product.id}/create/release/',
+            data=data,
+            format="json"
+        )
+        self.assertEqual(response.status_code, 201)
+        
+        response_json = response.json()
+        self.assertEqual(response_json["description"], "Apenas um testezinho")
+
+    def test_create_two_releases_with_conflicting_dates(self):
+        release1 = {
+            "release_name": "testezada 1",
+            "start_at": "2023-11-24",
+            "end_at": "2023-11-30",
+            "goal": self.goal.id,
+            "description": "Essa tem que dar certo"
+        }
+
+        release2 = {
+            "release_name": "testezada 2",
+            "start_at": "2023-11-29",
+            "end_at": "2023-12-03",
+            "goal": self.goal.id,
+            "description": "Essa tem que dar errado"
+        }
+
+        response_release1 = self.client.post(
+            path=f'/api/v1/organizations/{self.org.id}/products/{self.product.id}/create/release/',
+            data=release1,
+            format="json"
+        )
+
+        response_release2 = self.client.post(
+            path=f'/api/v1/organizations/{self.org.id}/products/{self.product.id}/create/release/',
+            data=release2,
+            format="json"
+        )
+
+        self.assertEqual(response_release1.status_code, 201)
+        self.assertEqual(response_release2.status_code, 400)
+
+        self.assertEqual(
+            response_release2.json()["message"], 
+            "The start date must be greater than the start date of the previous release"
+        )
+
+    def test_create_two_releases_with_conflicting_names(self):
+        release1 = {
+            "release_name": "testezada do baum",
+            "start_at": "2023-11-24",
+            "end_at": "2023-11-30",
+            "goal": self.goal.id,
+            "description": "Essa tem que dar certo"
+        }
+
+        release2 = {
+            "release_name": "testezada do baum",
+            "start_at": "2023-12-01",
+            "end_at": "2023-12-03",
+            "goal": self.goal.id,
+            "description": "Essa tem que dar errado"
+        }
+
+        response_release1 = self.client.post(
+            path=f'/api/v1/organizations/{self.org.id}/products/{self.product.id}/create/release/',
+            data=release1,
+            format="json"
+        )
+
+        response_release2 = self.client.post(
+            path=f'/api/v1/organizations/{self.org.id}/products/{self.product.id}/create/release/',
+            data=release2,
+            format="json"
+        )
+
+        self.assertEqual(response_release1.status_code, 201)
+        self.assertEqual(response_release2.status_code, 400)
+
+        self.assertEqual(
+            response_release2.json()["message"], 
+            "The release name must be unique"
+        )
+
+    def test_create_releases_without_name(self):
+        data = {
+            "start_at": "2023-11-24",
+            "end_at": "2023-11-30",
+            "goal": self.goal.id
+        }
+
+        response = self.client.post(
+            path=f'/api/v1/organizations/{self.org.id}/products/{self.product.id}/create/release/',
+            data=data,
+            format="json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_releases_without_goal(self):
+        data = {
+            "release_name": "testezada do baum",
+            "start_at": "2023-11-24",
+            "end_at": "2023-11-30"
+        }
+
+        response = self.client.post(
+            path=f'/api/v1/organizations/{self.org.id}/products/{self.product.id}/create/release/',
+            data=data,
+            format="json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_releases_without_dates(self):
+        data = {
+            "release_name": "testezada do baum",
+            "goal": self.goal.id,
+            "description": "Essa tem que dar errado"
+        }
+
+        response = self.client.post(
+            path=f'/api/v1/organizations/{self.org.id}/products/{self.product.id}/create/release/',
+            data=data,
+            format="json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    
