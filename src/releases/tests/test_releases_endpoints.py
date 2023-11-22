2391a1f59a21c64cbe66a686644c4d4377180817
LamequeFernandes <lamequesao@gmail.com>
2023-11-22 18:05:39 -0300

    test: addition of sonar tests and correction in imports

--- src/releases/tests/test_releases_endpoints.py
+++ src/releases/tests/test_releases_endpoints.py
@@ -1,24 +1,11 @@
-# testes para releases
-
 from datetime import date, timedelta
-from urllib import request
 
-from django.contrib.auth import get_user_model
-from django.core.management import call_command
-from django.test import TestCase
 from rest_framework.authtoken.models import Token
-from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
 
 from goals.models import Goal
-from organizations.management.commands.utils import (
-    create_a_preconfig,
-    create_suported_characteristics,
-)
-from organizations.models import Product, Repository
 from releases.models import Release
 from utils.tests import APITestCaseExpanded
-from rest_framework import status
-from rest_framework.test import APIClient
 
 
 class ReleaseEndpointsTestCase(APITestCaseExpanded):
